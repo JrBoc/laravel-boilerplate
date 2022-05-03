af853dec2db53b3b45e74fd15bcbde7567f912e1
John Rhey <jrboc18@gmail.com>
2022-05-03 14:10:02 +0800

    added js and scss files via npm.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
-require('./bootstrap');
+try {
+    window.$ = window.jQuery = require('jquery');
+    window.coreui = require('@coreui/coreui');
+
+    require('@popperjs/core');
+    require('bootstrap');
+    require('simplebar');
+
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+} catch (e) {
+    console.error(e)
+}
